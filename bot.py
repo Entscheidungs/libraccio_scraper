768a822370371d65cf8fca1a2523c576af77ddc8
Entscheidungs <54262504+Entscheidungs@users.noreply.github.com>
2023-09-14 12:32:59 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,9 +7,11 @@ from telegram import __version__ as TG_VER,Bot
 import json
 import asyncio
 import credentials
+import lista_libri
 
 _path = "/root/scraper_libraccio/lista_libri.json" 
 _path = "/home/chris/Documents/scraper_libraccio/lista_libri.json"
+
 
 try:
 
@@ -85,38 +87,29 @@ async def scraping() -> None:
         return stato != None,link ##ritorna True se il libro c'è usato, False se non c'è usato
     bot= Bot(token=credentials.token)
     while True:
-        while True:
-            try:
-                with open(_path,"r") as file: 
-                    _file = json.load(file)
-                copiafile = _file.copy() 
-                break
-            except:
-                time.sleep(2)
-        for id,lista in _file.items():
+        for id,lista in lista_libri.file.items():
             for idx,libro in enumerate(lista):
                 q = monitora(libro)
-#                print(libro)
+                #print(libro)
                 if q[0]:
-                    del copiafile[id][idx]
                     await bot.sendMessage(id,f"E' stato trovato un libro usato al seguente link: {q[1]}")
-        with open(_path,"w") as file: 
-            json.dump(copiafile,file)
+                    del lista_libri.file[id][idx]
+
+        
         await asyncio.sleep(1)
 def _scraping():
     asyncio.run(scraping())
 async def miei_libri(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-        with open(_path,"r") as file:  
-            x = json.load(file)
         
         userid = str(update.message.from_user["id"])
         s = ""
-        for x in x[userid]:
+        for x in lista_libri.file[userid]:
             s+=x + "\n\n"
 
         s = s[:-1]
         await update.message.reply_text("Ecco i libri che hai inserito nel database: \n" + s)
-        
+ 
+
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 
     """Send a message when the command /help is issued."""
@@ -144,15 +137,11 @@ async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
             return
         await update.message.reply_text("Il libro è stato salvato nel database, verrai avvisato non appena sarà disponibile usato")
         #! w+ non si può usare perché il contenuto del file prima viene azzerato e poi viene letto, per cui
-        with open(_path,"r") as file:  
-            x = json.load(file)
 
         with open(_path,"w") as file:
-            if x.get(userid) == None:
-                x[userid] = []
-            x[userid].append(update.message.text)
-            time.sleep(5)
-            json.dump(x,file)
+            if lista_libri.file.get(userid) == None:
+                lista_libri.file[userid] = []
+            lista_libri.file[userid].append(update.message.text)
 
             print(f"Aggiunto {update.message.text}")
 def main() -> None:
