9d9952e91dc64c0defbc1ed330e565488af63783
Entscheidungs <54262504+Entscheidungs@users.noreply.github.com>
2023-12-18 18:32:15 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -33,21 +33,18 @@ logger = logging.getLogger(__name__)
 
 # context.
 
-INSERT,SCRAPE = range(2)
+INSERT,SCRAPE, REQ= range(3)
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 
     """Send a message when the command /start is issued."""
 
-    user = update.effective_user
 
-    await update.message.reply_html(
-
-        rf"Hi {user.mention_html()}!",
-
-        reply_markup=ForceReply(selective=True),
-
-    )
+    await update.message.reply_text("Questo bot ti è utile nel caso tu voglia comprare un libro su libraccio.it usato, ma sul sito sia presente solamente nuovo.")
+    await update.message.reply_text("Inserendo il comando /ricerca ed inviando poi il link del libro in questione (attenzione, il link deve essere di un libro, non di un autore o altro), lo stato del libro verrà monitorato periodicamente dal bot, che ti avviserà quando sarà disponibile usato.")
+    await update.message.reply_text("Infine puoi visualizzare la lista dei libri che il bot sta monitorando tramite il comando /lista_libri.")
+    await update.message.reply_text("Questo bot è in fase di testing, il creatore si scusa per eventuali malfunzionamenti, per qualsiasi chiarimento siete pregati di contattare @zzeroxchris")
+    await update.message.reply_text("Il codice sorgente del bot è inoltre disponibile su github al link https://github.com/Entscheidungs/libraccio_scraper e se ti è tornato utile sei pregato di lasciare una star!")
 
 
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -73,7 +70,16 @@ async def req(update: Update, context: ContextTypes.DEFAULT_TYPE): # it asks to 
 async def inserting(update: Update, context: ContextTypes.DEFAULT_TYPE): # it asks to enter the link of the book
     link = update.message.text
 
-
+    try:
+        x = requests.get(link)
+        if "https://www.libraccio.it/libro/" not in link:
+            raise Exception
+    except:
+        await update.message.reply_text("Il link inserito non risulta valido, sicuro che sia il link completo di un libro?")
+        await update.message.reply_text("Inserisci il link del libro che vorresti trovare usato\n") 
+        return INSERT
+    
+    obj.acquire()
     with open(path,"r") as f: #the content of the json file is temporaily stored in a dictionary
         jsondict = json.load(f)
     userid = str(update.message.from_user["id"])
@@ -91,37 +97,45 @@ async def inserting(update: Update, context: ContextTypes.DEFAULT_TYPE): # it as
         jsondict[userid].append(link)
         with open(path,"w") as f: #the json file is updated
             json.dump(jsondict,f)
+            obj.release()
         await update.message.reply_text("Il libro è stato inserito nel database")
     return ConversationHandler.END
 def _scraping():
     asyncio.run(scraping())
+    
+    
+    
 async def scraping(): #this function scrapes the webpage of libraccio.it searching for the buybox-used div class
 
     bot= Bot(token=credentials.token)
     while True:
-       
+        obj.acquire()
         with open(path,"r") as f:
             jsondict = json.load(f)
         
         newdict = {} #the new dict will contain only the books who are not found 
-        for list_of_books in jsondict.keys():
-            newdict[list_of_books] = []
-            for book in jsondict[list_of_books]:
+        for userid in jsondict.keys():
+            newdict[userid] = []
+            for book in jsondict[userid]:
                 url = requests.get(book)
+
                 bs = BeautifulSoup(url.content,"html.parser")
-                stato = bs.find("div",class_="buybox-used")
-                if stato:
-                    try:
-                        await bot.send_message(text=f"Buone notizie, un libro che stavi cercando è stato trovato usato:\n{book}",chat_id=list_of_books)
-                    except:
-                        pass
+                success = bs.find("div",class_="buybox-used")
+                print("ENTRA LIBRO")
+                if success:
+
+                        print("ENTRA LIBRO 1") if "essere" in book else None
+                        await bot.send_message(text=f"Buone notizie, un libro che stavi cercando è stato trovato usato:\n{book}",chat_id=userid)
                 else:
-                    newdict[list_of_books].append(book)
-        
+                    print("ENTRA LIBRO 2") if "essere" in book else None
+                    newdict[userid].append(book)
         with open(path,"w") as f:
             json.dump(newdict,f)
-    
+
+        obj.release()
         await asyncio.sleep(5)
+
+
 
 async def show_list(update: Update, context: ContextTypes.DEFAULT_TYPE): # it asks to enter the link of the book
     userid = str(update.message.from_user["id"])
@@ -151,7 +165,7 @@ def main() -> None:
     # on different commands - answer in Telegram
 
     conv_handler = ConversationHandler(entry_points=[CommandHandler("start",start),CommandHandler("ricerca",req),CommandHandler("lista_libri",show_list)],
-    states = {INSERT : [MessageHandler(filters.TEXT, inserting)]},fallbacks=[],)
+    states = {INSERT : [MessageHandler(filters.TEXT, inserting)], REQ : [CommandHandler("ricerca",req)]},fallbacks=[],)
     
 
 
@@ -163,6 +177,8 @@ def main() -> None:
     application.run_polling(allowed_updates=Update.ALL_TYPES)
 if __name__ == "__main__":
 
+
+    obj = threading.Semaphore(1)
     _thread = threading.Thread(target=_scraping)
     _thread.start()
     while True:
